be2ab2906dd862a739f9af9e8966eadc98b2a042
alecbakholdin <alecbakholdin@gmail.com>
2025-01-04 09:59:58 -0500

    fixed lsp

--- lua/abakho/lazy/lsp.lua
+++ lua/abakho/lazy/lsp.lua
@@ -20,30 +20,60 @@ return {
 	    })
 
 	    local cmp = require('cmp')
+        local cmp_lsp = require('cmp_nvim_lsp')
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+        require('fidget').setup({})
 	    require('mason').setup()
 	    require('mason-lspconfig').setup({
-		ensure_installed = {
-			"lua_ls"
-		},
-		handlers = {
-			function(server_name)
-				require("lspconfig")[server_name].setup {
-					capabilities = capabilities
-				}
-			end,
-		},
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "gopls"
+            },
+            handlers = {
+                function(server_name)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+
+                ["lua_ls"] = function()
+                    local lspconfig = require('lspconfig')
+                    lspconfig.lua_ls.setup ({
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                                }
+                            }
+                        }
+                    })
+                end,
+            },
 	    })
 
 	    local cmp_select = { behavior = cmp.SelectBehavior.Select }
 	    cmp.setup({
-		    mapping = cmp.mapping.preset.insert({
-			['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-			['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-			['<C-y>'] = cmp.mapping.confirm({ select= true }),
-			['<C-Space>'] = cmp.mapping.complete(),
-		    }),
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<Tab>'] = cmp.mapping.confirm({ select= true }),
+                ['<C-Space>'] = cmp.mapping.complete(),
+            }),
 		    sources = cmp.config.sources({
-			    { name = 'nvim.lsp' },
+			    { name = 'nvim_lsp' },
 			    { name = 'luasnip' },
 		    }, {
 			    { name = 'buffer' },
