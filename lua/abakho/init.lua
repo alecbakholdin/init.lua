712d3145e57c7f2f0286057a75bb8311cb39e072
Alec Bakholdin <abakho@icims.com>
2025-01-09 16:43:08 -0500

    added conform with prettier

--- lua/abakho/init.lua
+++ lua/abakho/init.lua
@@ -3,49 +3,49 @@ require("abakho.lazy_init")
 require("abakho.set")
 
 local augroup = vim.api.nvim_create_augroup
-local AbakhoGroup = augroup('Abakho', {})
+local AbakhoGroup = augroup("Abakho", {})
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
-    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-    -- machine and codebase, you may want longer. Add an additional
-    -- argument after params if you find that you have to write the file
-    -- twice for changes to be saved.
-    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-    for cid, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-          vim.lsp.util.apply_workspace_edit(r.edit, enc)
-        end
-      end
-    end
-    vim.lsp.buf.format({async = false})
-  end
-})
+--autocmd("BufWritePre", {
+--pattern = "*.go",
+--callback = function()
+--local params = vim.lsp.util.make_range_params()
+--params.context = { only = { "source.organizeImports" } }
+---- buf_request_sync defaults to a 1000ms timeout. Depending on your
+---- machine and codebase, you may want longer. Add an additional
+---- argument after params if you find that you have to write the file
+---- twice for changes to be saved.
+---- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+--local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+--for cid, res in pairs(result or {}) do
+--for _, r in pairs(res.result or {}) do
+--if r.edit then
+--local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+--vim.lsp.util.apply_workspace_edit(r.edit, enc)
+--end
+--end
+--end
+--vim.lsp.buf.format({ async = false })
+--end,
+--})
 
-autocmd('LspAttach', {
-    group = AbakhoGroup,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        -- I know how to use these
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("v", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<hleader>rr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("i", "<C-Space>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+autocmd("LspAttach", {
+	group = AbakhoGroup,
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		-- I know how to use these
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts)
+		vim.keymap.set({ "i", "n" }, "<leader>sh", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>er", vim.diagnostic.open_float, opts)
 
-        -- I don't know what these do yet. We'll get there one day
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    end
+		-- I don't know what these do yet. We'll get there one day
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+	end,
 })
--- lua/abakho/lazy/lsp.lua
+++ lua/abakho/lazy/lsp.lua
@@ -1,107 +1,133 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "stevearc/conform.nvim",
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-    },
-    config = function()
-	    require("conform").setup({
-		    formatters_by_ft = {
-		    }
-	    })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"stevearc/conform.nvim",
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"j-hui/fidget.nvim",
+	},
+	config = function()
+		local conform = require("conform")
+		local format_action_config = {
+			lsp_fallback = true,
+			async = false,
+		}
+		require("conform").setup({
+			formatters = {},
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				go = { "goimports" },
+			},
+			format_on_save = format_action_config,
+		})
+		vim.keymap.set({ "n", "v" }, "<leader>ff", function()
+			conform.format(format_action_config)
+		end, { desc = "Format file or range (in visual mode)" })
 
-	    local cmp = require('cmp')
-        local cmp_lsp = require('cmp_nvim_lsp')
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
-        require('fidget').setup({})
-	    require('mason').setup()
-	    require('mason-lspconfig').setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "gopls"
-            },
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
+		require("fidget").setup({})
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"rust_analyzer",
+				"gopls",
+				"golangci_lint_ls",
+				"emmet_ls",
+				"tailwindcss",
+				"html",
+			},
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
 
-                ["lua_ls"] = function()
-                    local lspconfig = require('lspconfig')
-                    lspconfig.lua_ls.setup ({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    })
-                end,
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								runtime = { version = "Lua 5.1" },
+								diagnostics = {
+									globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+								},
+							},
+						},
+					})
+				end,
 
-                ["tailwindcss"] = function()
-                    local lspconfig = require('lspconfig')
-                    lspconfig.tailwindcss.setup ({
-                        capabilities = capabilities,
-                        root_dir = function(fname)
-                            local root_path = require('lspconfig').util.root_pattern(
-                                'tailwind.config.*'
-                            )
-                            return root_path(fname)
-                        end
-                    })
-                end
-            },
-	    })
+				["tailwindcss"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.tailwindcss.setup({
+						capabilities = capabilities,
+						root_dir = function(fname)
+							local root_path = require("lspconfig").util.root_pattern("tailwind.config.*")
+							return root_path(fname)
+						end,
+					})
+				end,
+			},
+		})
 
-	    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-	    cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<Tab>'] = cmp.mapping.confirm({ select= true }),
-                ['<C-Space>'] = cmp.mapping.complete(),
-            }),
-		    sources = cmp.config.sources({
-			    { name = 'nvim_lsp' },
-			    { name = 'luasnip' },
-		    }, {
-			    { name = 'buffer' },
-		    })
-	    })
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<Tab>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			}, {
+				{ name = "buffer" },
+			}),
+		})
 
-	    vim.diagnostic.config({
-		    float = {
-			    focusable = false,
-			    style = "minimal",
-			    border = "rounded",
-			    source = "always",
-			    header = "",
-			    prefix = "",
-		    },
-	    })
-    end
+		vim.diagnostic.config({
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
+	end,
 }
